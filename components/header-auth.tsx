759b2c2131939e110716d4d510d1d601d6ebc968
Duc Nguyen <dnguyensde@gmail.com>
2025-04-26 18:57:22 -0400

    [SnackTrack - 1.0.0] fix: tailwind.config consistent screen size between local and deployed

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,6 +1,5 @@
 import { signOutAction } from "@/app/server-actions/auth/actions";
 import Link from "next/link";
-import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
 
