6ed4ed6e75ad96ef92c2f69b06cb56c19e14c72d
Duc Nguyen <dnguyensde@gmail.com>
2025-06-04 13:40:22 -0400

    [SnackTrack - 1.0.0] chore: updated sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,16 @@
 import type { MetadataRoute } from "next";
 import { fetchSnackIds } from "./server-actions/snacks/actions";
+import { encodeId } from "@/utils/hashids";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const snackIds = await fetchSnackIds();
+  const encodedSnackIds = snackIds?.map((snackId) => encodeId(snackId));
 
   const defaultUrl =
     process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
 
   const snackEntries: MetadataRoute.Sitemap =
-    snackIds?.map((id) => ({
+    encodedSnackIds?.map((id) => ({
       url: `${defaultUrl}/snacks/${id}`,
       lastModified: new Date(), // required
       changeFrequency: "weekly", // required
