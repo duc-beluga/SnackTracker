5d89e2ccbae236c12fe32ac63bc53a3672907260
Duc Nguyen <dnguyensde@gmail.com>
2025-06-10 13:17:37 -0400

    [SnackTrack - 1.0.0] fix: custom csp for different env

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const env = process.env.NODE_ENV || "production";
+const isDev = env === "development";
+
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-inline' ${isDev ? " 'unsafe-eval'" : ""};;
   style-src 'self' 'unsafe-inline';
   img-src 'self' https://yjzinpfmkbsbtxcosviy.supabase.co https://lh3.googleusercontent.com data: blob:;
   font-src 'self';
