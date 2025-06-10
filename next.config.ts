2d1acc5af77c609dad984c0420da0d266f285830
Duc Nguyen <dnguyensde@gmail.com>
2025-06-09 23:37:13 -0400

    [SnackTrack - 1.0.0] security: add 'unsafe-inline' back since we're using third-party script

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self';
+  script-src 'self' 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
   img-src 'self' https://yjzinpfmkbsbtxcosviy.supabase.co https://lh3.googleusercontent.com data: blob:;
   font-src 'self';
